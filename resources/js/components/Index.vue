c85738443b2c4d3c33f6afa12bc9ab23059c9309
Firdavs Ibodullaev <firdavs3253@gmail.com>
2024-02-16 00:09:53 +0500

    installed vue application

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+Salom
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Index2.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "Index2"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
